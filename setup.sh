f5301f5973bc927eb83bbab721d1a1056a0b7897
Chai Phonbopit <Phonbopit@gmail.com>
2016-07-26 12:06:13 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,12 @@
 
 # Script to install Git, Node.js onto a 
 # Ubuntu System.
+# curl -sL https://raw.githubusercontent.com/Phonbopit/setup-ubuntu.sh/master/setup.sh | sudo -E bash -
 
-echo "Test..."
+apt-get -y install git
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
+apt-get install -y build-essential
+
+echo "################ Completed!!!!! ################"
